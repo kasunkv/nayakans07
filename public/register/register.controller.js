c960815dfe9c183383c2ab2755591c6b34a8f2be
Kasun Kodagoda <kvkrusl@gmail.com>
2015-07-26 20:02:57 +0530

    Added register view and the controller

---
+++ public/register/register.controller.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+	angular
+		.module('nayakans07')
+		.controller('RegisterController', registerController);
+
+	function registerController ($scope) {
+		
+	}
+
+})();
