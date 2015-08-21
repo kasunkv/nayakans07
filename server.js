94b8a189e81892b4b8dbb4c22d6067a1fa097cb9
Kasun Kodagoda <kvkrusl@gmail.com>
2015-08-22 03:33:43 +0530

    Add cors package and setup CORS on server

--- server.js
+++ server.js
@@ -4,11 +4,23 @@
 	var express = require('express');
 	var mongoose = require('mongoose');
 	var bodyParser = require('body-parser');
+	var cors = require('cors');
 	var config = require('./api/config/configuration.js');
 
 	var app = express();
 	var port = process.env.PORT || 8080;
 	var appRoutes = require('./api/routes.js');
+
+	// setup CORS
+	var whitelist = ['http://localhost:8080', 'http://ec2-54-148-6-208.us-west-2.compute.amazonaws.com:8080'];
+	app.use(cors({
+	    origin: function(origin, callback) {
+	        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+	        callback(null, originIsWhitelisted);
+	    },
+	    credentials: true,
+	    allowedHeaders: ['Content-Type', 'Authorization' ]
+	}));
 
 	// mongodb config
 	mongoose.connect(config.MONGO_URL);
